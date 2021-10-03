fc9200f94be05e87843f8139ba4886e9ebf90cdb
dillionmegida <dillionmegida@gmail.com>
2021-10-03 23:50:53 +0100

    feat: start building roadmap page

---
+++ src/pages/roadmap.tsx
@@ -0,0 +1,13 @@
+import Heading from '@/atoms/heading'
+import Layout from '@/organisms/layout'
+import * as React from 'react'
+
+export default function Roadmap() {
+  return (
+    <Layout>
+      <div className="container">
+        <Heading level={1}>Roadmap</Heading>
+      </div>
+    </Layout>
+  )
+}
