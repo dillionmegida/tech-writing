4382d7263b4a6c6aea23ff3ba77f138db27df31c
dillionmegida <dillionmegida@gmail.com>
2021-10-16 18:18:09 +0100

    feat: update domain name constant

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,4 +1,4 @@
-export const DOMAIN = 'https://tech-writing.netlify.app'
+export const DOMAIN = 'https://writing.dillionmegida.com'
 export const LOGO_PATH = '/logo.png'
 
 export const INTRODUCTION_COVER = '/assets/images/introduction-cover.png'
